b4ecae6ff0fa838acc5c752971b4a84d4be3eb53
vidaxd <Usuario@DESKTOP-MAOPI2H>
2019-11-22 19:19:23 +0000

    Renombrada tablas y columnas de la BD para que sea en ingles, añadida la clase DDBBConnection y su clase de pruebas, esta clase permite añadir, obtener, y eliminar un usuario.

---
+++ src/control/DDBBConection.java
@@ -0,0 +1,151 @@
+package control;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.User;
+import view.VistaPrincipal;
+
+public class DDBBConection {
+    
+    private Connection con;
+    private DatabaseMetaData dbmd;
+    
+    public void connect() {
+        String servidorSQLitePrueba = "jdbc:sqlite:Base de datos GS1.db";
+        
+        try {
+            con = DriverManager.getConnection(servidorSQLitePrueba);
+            dbmd = con.getMetaData();
+        } catch (SQLException ex) {
+            Logger.getLogger(VistaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void addUser(String name, String lastname, int age, String genre, String mail, String rol, String password) {
+        String sql = "INSERT INTO User(name, lastname, age, genre, mail, rol, passwordkey) VALUES (?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1,name);
+            ps.setString(2, lastname);
+            ps.setInt(3, age);
+            ps.setString(4, genre);
+            ps.setString(5, mail);
+            ps.setString(6, rol);
+            ps.setString(7, password);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DDBBConection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void addUser(String name, String lastname, int age, float weight, float height, String genre, String mail, String rol, String password) {
+        String sql = "INSERT INTO User(name, lastname, age, weight, height, genre, mail, rol, passwordkey) VALUES (?,?,?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1,name);
+            ps.setString(2, lastname);
+            ps.setInt(3, age);
+            if(weight > 0.0f) {
+               ps.setFloat(4, weight);
+            } else {
+               ps.setNull(4, java.sql.Types.NULL);
+            }
+            if(height > 0.0f) {
+                ps.setFloat(5, height);
+            } else {
+                ps.setNull(5, java.sql.Types.NULL);
+            }
+            ps.setString(6, genre);
+            ps.setString(7, mail);
+            ps.setString(8, rol);
+            ps.setString(9, password);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DDBBConection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void deleteUserByMail(String mail) {
+        String sql = "DELETE FROM User WHERE Mail=?;";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1,mail);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DDBBConection.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public User getUserByMail(String mail) {
+        String sql = "SELECT * FROM User WHERE Mail=\"" + mail + "\"";
+        User u = null;
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                u = new User(rs.getString("name"), rs.getString("lastname"), rs.getInt("age"), 
+                        rs.getFloat("weight"), rs.getFloat("height"), rs.getString("genre"), 
+                        rs.getString("mail"), rs.getString("rol"), rs.getString("passwordkey"));
+            }
+            stmt.close();
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DDBBConection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return u;
+    }
+    
+    public List<String> getTables() {
+        List<String> tablas = new ArrayList<>();
+        try {
+            String[] types = {"TABLE"};
+            ResultSet rs = dbmd.getTables(null, null, "%", types);
+            while (rs.next()) {
+                String nombreTabla = rs.getString("TABLE_NAME");
+                //System.out.println("Tabla: " + nombreTabla);
+                tablas.add(nombreTabla);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(VistaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return tablas;
+    }
+
+    public List<String> getColumnsByTable(String tabla) {
+        List<String> columnas = new ArrayList<>();
+        try {
+            ResultSet rs = dbmd.getColumns(null, null, tabla, null);
+            while (rs.next()) {
+                String nombreCampo = rs.getString("COLUMN_NAME");
+                //System.out.println("   Campo: " + nombreCampo);
+                columnas.add(tabla + "." + nombreCampo);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(VistaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return columnas;
+    }
+    
+    public void closeConnection() {
+        if(con != null) try {
+            con.close();
+            con = null;
+        } catch (SQLException ex) {
+            Logger.getLogger(VistaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
